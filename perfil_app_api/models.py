e8633a9229f535756421af0c8a45a8d63d1f0c9f
wild320 <you@example.com>
2022-02-12 10:09:54 -0500

    Migracion, cambio variable username

--- perfil_app_api/models.py
+++ perfil_app_api/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    def create(self,tipo_documento,documento,nombres,apellidos):
+    def create(self,tipo_documento,documento,nombres,apellidos,hobbie):
         if not documento:
             raise ValueError("Debes tener un Numero de documento")
         
@@ -16,23 +16,20 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-   tipo_documento = models.CharField(max_length=255)
-   documento = models.IntegerField(max_length=255, unique=True)   
-   nombres =models.CharField(max_length=255)   
-   apellidos =models.CharField(max_length=255) 
-   hobbie =models.CharField(max_length=255)
-   activado=models.BooleanField(default=True)
-
-   objects = UserManager()
-
-   DOCUMENTO_FIELD = 'documento'
-   REQUIRED_FIELD = ['nombres']
-
-   def get_full_name(self):
+    tipo_documento = models.CharField(max_length=255)
+    documento = models.IntegerField(unique=True)
+    nombres =models.CharField(max_length=255)   
+    apellidos =models.CharField(max_length=255)
+    hobbie =models.CharField(max_length=255)
+    activado=models.BooleanField(default=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'documento'
+    REQUIRED_FIELD = ['nombres']
+    
+    def get_full_name(self):
        return self.nombres 
-
-   def get_short_name(self):
+    
+    def get_short_name(self):
        return self.nombres
-
     def __str__(self):
         return self.documento
